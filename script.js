b161fd93a8ecdcd3ebb8d5af521d957602dcd252
Xraygrrl7 <xraygrrl7@yahoo.com>
2025-03-20 12:45:55 +0100

    'Initial commit'
    Moved out of virtual box to my computer with code I previously had completed.

---
+++ script.js
@@ -0,0 +1,75 @@
+/*Generate a random number between 0 and 1
+If number is < 0.33 rock
+else if number is < 0.66 paper
+else scissors */
+
+function getComputerChoice() {
+	let num = Math.random();
+	console.log(num);
+
+	if (num < 0.33) {
+		return 'rock';
+	} else if (num < 0.66) {
+		return 'paper';
+	} else {
+		return 'scissors';
+	}
+}
+
+console.log(getComputerChoice);
+
+// Part 2 logic for getting human choice
+/* function getHumanChoice
+prompt user 'type rock, paper, or scissors to play'
+store choice
+end function*/
+
+function getHumanChoice() {
+	let choice;
+
+	while (true) {
+		choice = prompt('To play type rock, paper, or scissors').toLowerCase();
+		if (choice === 'rock' || choice === 'paper' || choice === 'scissors') {
+			return choice;
+		} else {
+			alert('Invalid choice! Please enter rock, paper, or scissors.');
+		}
+	}
+}
+// console.log(getHumanChoice);
+/*Part 3 declare player score variables */
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+/*Part 4 Write logic to play single round */
+
+function playRound(humanChoice, computerChoice) {
+	console.log(
+		`playRound is running with Human: ${humanChoice}, computer: ${computerChoice}`
+	);
+
+	if (humanChoice === computerChoice) {
+		alert('It is a draw');
+	} else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+		alert('You win, scissors beats paper!');
+	} else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+		alert('You lose, rock beats scissors!');
+	} else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+		alert('You lose, scissors beats paper!');
+	} else if (humanChoice === 'paper' && computerChoice === 'rock') {
+		alert('You win, paper beats rock!');
+	} else if (humanChoice === 'rock' && computerChoice === 'paper') {
+		alert('You lose, paper beats rock!');
+	} else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+		alert('You win, rock beats scissors!');
+	} else {
+		alert('Invalid choice, please enter rock, paper, or scissors.');
+	}
+}
+
+const humanSelection = getHumanChoice();
+
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
